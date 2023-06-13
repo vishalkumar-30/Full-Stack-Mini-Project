692a55ee95a5caea14748f2af117e19efc4358fc
vishalkumar-30 <vishal1924s@gmail.com>
2023-06-13 13:33:28 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crud",
+});
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM student";
+  db.query(sql, (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
+});
+
+app.post("/create", (req, res) => {
+  const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?)";
+  const values = [req.body.name, req.body.email];
+  db.query(sql, [values], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
+});
+
+app.put("/update/:id", (req, res) => {
+  console.log("/update/:id");
+  const sql = "update student set `Name` = ?, `Email` = ? where ID = ?";
+  const values = [req.body.name, req.body.email];
+  const id = req.params.id;
+  console.log(req);
+
+  db.query(sql, [...values, id], (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "An error occurred" });
+    }
+    return res.json(data);
+  });
+});
+
+app.delete("/student/:id", (req, res) => {
+  const sql = "DELETE FROM student WHERE ID = ?";
+
+  const id = req.params.id;
+
+  db.query(sql, [id], (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "An error occurred" });
+    }
+    return res.json(data);
+  });
+});
+
+app.listen(8001, () => {
+  console.log("app is listening on port 8001");
+});
